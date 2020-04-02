c0a4024ecee5acd98c84286eb76e886a0702c917
SheilaBirgen <jeronobergen@gmail.com>
2020-04-02 19:56:27 +0300

    add postgres

--- myhood/settings.py
+++ myhood/settings.py
@@ -96,8 +96,7 @@ WSGI_APPLICATION = 'myhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
