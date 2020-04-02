953dbe03c22990b8aad6c78cf2df8497e793be18
SheilaBirgen <jeronobergen@gmail.com>
2020-04-02 19:56:12 +0300

    add method edit profile

--- hoodnews/views.py
+++ hoodnews/views.py
@@ -6,24 +6,7 @@ from rest_framework.views import APIView
 from .permissions import IsAdminOrReadOnly
 from django.contrib.auth import login, logout
 from .models import *
-from rest_framework import generics, status
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.permissions import IsAuthenticated
-
-# @csrf_exempt
-# def get_data(request):
-# 	data = ExampleModel.objects.all()
-# 	if request.method == 'GET':
-# 		serializer = ExampleModelSerializer(data, many=True)
-# 		return JsonResponse(serializer.data, safe=False)
-
-
-
-class UserCreate(APIView):
-    authentication_classes = ()
-    permission_classes = ()
-    serializer_class = UserSerializer
-
 
 class LoginView(APIView):
     permission_classes = ()
@@ -38,49 +21,17 @@ class LoginView(APIView):
             return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
 
 
-class UserList(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class ProfileList(APIView):
-    def get_queryset(self):
-        queryset = Profile.objects.filter(user_id=self.kwargs["pk"])
-        return queryset
-
-    serializer_class = ProfileSerializer
-
-    def put(self, request, *args, **kwargs):
-        profile = Profile.objects.get(pk=self.kwargs["pk"])
-        serializer = ProfileSerializer(profile, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class EditHoodInfo(generics.RetrieveUpdateAPIView):
-    def get_queryset(self):
-        queryset = Neighbourhood.objects.filter(pk=self.kwargs["pk"])
-        return queryset
-
-    serializer_class = NeighbourhoodSerializer
-
-    def put(self, request, *args, **kwargs):
-        system_admin = SystemAdmin.objects.get(pk=self.kwargs["id"])
-        if system_admin.is_admin:
-            hood = Neighbourhood.objects.get(pk=self.kwargs["pk"])
-            serializer = NeighbourhoodSerializer(hood, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        else:
-            return Response({"Not authorized"}, status=status.HTTP_400_BAD_REQUEST)
-
-
+class UserList(APIView):
+    def get(self, request, format=None):
+        all_users = User.objects.all()
+        serializers = UserSerializer(all_users,many=True)
+        return Response(serializers.data)
+    def post(self, request,format=None):
+        serializers = UserSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class NeighbourhoodList(APIView):
     permission_classes = (IsAuthenticated,)
@@ -153,17 +104,24 @@ class CreateDepartmentView(APIView):
             return Response(serializers.data,status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class DeleteHood(generics.DestroyAPIView):
-    def destroy(self, request, *args, **kwargs):
-        system_admin = SystemAdmin.objects.get(pk=self.kwargs["id"])
-        if system_admin.is_admin:
-            try:
-                queryset = Neighbourhood.objects.get(pk=self.kwargs["pk"])
-            except ObjectDoesNotExist:
-                return Response({"Neighbourhood does not exist"}, status=status.HTTP_400_BAD_REQUEST)
-
-            queryset.delete()
-            return Response({"Neighbourhood deleted"})
-
-        else:
-            return Response({"Not authorized"}, status=status.HTTP_400_BAD_REQUEST)
+class EditProfileView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def get_profile(self, pk):
+        try:
+            return Profile.objects.get(pk=pk)
+        except Profile.DoesNotExist:
+            return Http404
+    def get_hood(self, id):
+        try:
+            return Neighbourhood.objects.get(id=id)
+        except Neighbourhood.DoesNotExist:
+            return Http404
+    def put(self, request,pk,id,format=None):
+        profile = self.get_profile(pk)
+        hood = self.get_hood(id)
+        serializers = ProfileSerializer(profile,data=request.data)
+        if serializers.is_valid():
+            serializers.save(user=request.user, neighbourhood=hood)
+            return Response(serializers.data)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
