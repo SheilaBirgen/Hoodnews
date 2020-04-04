f48b864e1e283797d9b6e4ba5b93d439c702542c
SheilaBirgen <jeronobergen@gmail.com>
2020-04-04 17:40:59 +0300

    add simple jwt authentication session

--- myhood/settings.py
+++ myhood/settings.py
@@ -48,11 +48,13 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAdminUser', 
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     
 }
