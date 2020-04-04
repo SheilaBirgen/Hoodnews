fed66433597b9822bac2e70b222c1d9c8455d264
SheilaBirgen <jeronobergen@gmail.com>
2020-04-04 16:55:38 +0300

    registered department on admin.py

--- hoodnews/admin.py
+++ hoodnews/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Neighbourhood, Profile,Business,User
+from .models import Neighbourhood, Profile,Business,User,Department
 
 # Register your models here.
 admin.site.register(Neighbourhood)
 admin.site.register(Profile)
 admin.site.register(Business)
 admin.site.register(User)
+admin.site.register(Department)
